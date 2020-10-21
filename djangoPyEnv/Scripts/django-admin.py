8b62b4958b5ad2e08872553d616bd3c3cb275957
Your Name <you@example.com>
2020-10-20 20:31:21 -0400

    initial commit

---
+++ djangoPyEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\15189\desktop\python_stack\my_enviorments\djangopyenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
